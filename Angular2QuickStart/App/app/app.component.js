4d9474788de247f61da9965f0fd892878453e939
ssoong <none@none>
2016-07-07 16:24:16 -0700

    Added sample angular 2 quickstart

---
+++ Angular2QuickStart/App/app/app.component.js
@@ -0,0 +1,16 @@
+﻿//component schema
+//app.AppComponent =
+//   ng.core.Component({
+//   })
+//   .Class({
+//   });
+(function (app) {
+	app.AppComponent =
+	  ng.core.Component({
+	  	selector: 'my-app',
+	  	template: '<h1>Angular 2 Component</h1>'
+	  })
+	  .Class({
+	  	constructor: function () { }
+	  });
+})(window.app || (window.app = {}));
---
+++ Angular2QuickStart/App/app/main.js
@@ -0,0 +1,6 @@
+﻿(function (app) {
+    document.addEventListener('DOMContentLoaded', function () {
+        ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+        ng.platformBrowserDynamic.bootstrap(app.AppComponent2);
+    });
+})(window.app || (window.app = {}));
